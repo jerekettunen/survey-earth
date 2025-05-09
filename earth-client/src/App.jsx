bb6ab80ec7d0b07bae2f2f08eafe5c5746729d4c
jerekettunen <jereatkettunen@gmail.com>
2025-05-09 15:08:20 +0300

    client inti

---
+++ earth-client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
