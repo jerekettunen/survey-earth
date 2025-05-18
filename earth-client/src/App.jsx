3530f4049d9551698c525a10603948fc7ef581e7
jerekettunen <jereatkettunen@gmail.com>
2025-05-18 15:58:12 +0300

    sidebar functionality

--- earth-client/src/App.jsx
+++ earth-client/src/App.jsx
@@ -4,12 +4,14 @@ import { Routes, Route, useMatch, Link, Navigate } from 'react-router-dom'
 import { useApolloClient } from '@apollo/client'
 
 import { ThemeProvider } from './components/theme-provider'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import ModeToggle from './components/mode-toggle'
 import NewProjectForm from './components/NewProjectForm'
 import Projects from './components/Projects'
 import ProjectSingle from './components/ProjectSingle'
 import LoginForm from './components/LoginForm'
 import SignUpForm from './components/SignUpForm'
+import DashSideBar from './components/DashSideBar'
 
 import { Toaster } from '@/components/ui/sonner'
 import { toast } from 'sonner'
@@ -49,60 +51,66 @@ const App = () => {
 
   return (
     <ThemeProvider defaultTheme="system" storageKey="earth-client-theme">
-      <div className="flex flex-col items-center min-h-screen">
-        <div className="flex flex-wrap justify-center gap-4 mt-6 mb-8">
-          <Button asChild>
-            <Link to="/">Home</Link>
-          </Button>
-
-          {token ? (
-            // Show these links only when logged in
-            <>
-              <Button asChild>
-                <Link to="/projects">Projects</Link>
-              </Button>
-              <Button asChild>
-                <Link to="/add">Add Project</Link>
-              </Button>
-              <Button onClick={handleLogout}>Logout</Button>
-            </>
-          ) : (
-            // Show only login when not logged in
+      <SidebarProvider>
+        <DashSideBar />
+        <div className="flex flex-col items-center min-h-screen">
+          <SidebarTrigger />
+          <div className="flex flex-wrap justify-center gap-4 mt-6 mb-8">
             <Button asChild>
-              <Link to="/login">Login</Link>
+              <Link to="/">Home</Link>
             </Button>
-          )}
-          <ModeToggle />
-        </div>
-        <div className="flex flex-col items-center w-full max-w-4xl p-4">
-          <Routes>
-            <Route path="/" element={<div>Home</div>} />
-            <Route path="/projects" element={<Projects />} />
-            <Route
-              path="/projects/:id"
-              element={<ProjectSingle id={projectId} />}
-            />
-            <Route path="/add" element={<NewProjectForm />} />
-            <Route path="/login" element={<LoginForm setToken={setToken} />} />
-            <Route
-              path="/register"
-              element={<SignUpForm setToastMessage={setToastMessage} />}
-            />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </div>
 
-        <Toaster
-          toastOptions={{
-            className: 'sonner-toast',
-            style: {
-              background: 'var(--background)',
-              color: 'var(--text)',
-            },
-          }}
-          richColors
-        />
-      </div>
+            {token ? (
+              // Show these links only when logged in
+              <>
+                <Button asChild>
+                  <Link to="/projects">Projects</Link>
+                </Button>
+                <Button asChild>
+                  <Link to="/add">Add Project</Link>
+                </Button>
+                <Button onClick={handleLogout}>Logout</Button>
+              </>
+            ) : (
+              // Show only login when not logged in
+              <Button asChild>
+                <Link to="/login">Login</Link>
+              </Button>
+            )}
+            <ModeToggle />
+          </div>
+          <div className="flex flex-col items-center w-full max-w-4xl p-4">
+            <Routes>
+              <Route path="/" element={<div>Home</div>} />
+              <Route path="/projects" element={<Projects />} />
+              <Route
+                path="/projects/:id"
+                element={<ProjectSingle id={projectId} />}
+              />
+              <Route path="/add" element={<NewProjectForm />} />
+              <Route
+                path="/login"
+                element={<LoginForm setToken={setToken} />}
+              />
+              <Route
+                path="/register"
+                element={<SignUpForm setToastMessage={setToastMessage} />}
+              />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </div>
+          <Toaster
+            toastOptions={{
+              className: 'sonner-toast',
+              style: {
+                background: 'var(--background)',
+                color: 'var(--text)',
+              },
+            }}
+            richColors
+          />
+        </div>
+      </SidebarProvider>
     </ThemeProvider>
   )
 }
---
+++ earth-client/src/components/DashSideBar.jsx
@@ -0,0 +1,63 @@
+import { Home, Inbox, Search, Settings, Plus } from 'lucide-react'
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+
+// Menu items.
+const items = [
+  {
+    title: 'Home',
+    url: '#',
+    icon: Home,
+  },
+  {
+    title: 'Projects',
+    url: '/projects',
+    icon: Inbox,
+  },
+  {
+    title: 'Add Project',
+    url: '/add',
+    icon: Plus,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+]
+const DashSideBar = () => {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
+
+export default DashSideBar
